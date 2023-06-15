22267b91b5ebe09ec2de882ab41548d29a37d722
Kalarani <lkalarani@gmail.com>
2023-06-15 09:20:49 +0530

    Cleans up classes in energy domain. Moves required files to offboarding domain

---
+++ src/main/java/uk/tw/offboarding/generator/TestDataGenerator.java
@@ -0,0 +1,20 @@
+package uk.tw.energy.generator;
+
+public class ElectricityReadingsGenerator {
+
+//    public List<ElectricityReading> generate(int number) {
+//        List<ElectricityReading> readings = new ArrayList<>();
+//        Instant now = Instant.now();
+//
+//        Random readingRandomiser = new Random();
+//        for (int i = 0; i < number; i++) {
+//            double positiveRandomValue = Math.abs(readingRandomiser.nextGaussian());
+//            BigDecimal randomReading = BigDecimal.valueOf(positiveRandomValue).setScale(4, RoundingMode.CEILING);
+//            ElectricityReading electricityReading = new ElectricityReading(now.minusSeconds(i * 10), randomReading);
+//            readings.add(electricityReading);
+//        }
+//
+//        readings.sort(Comparator.comparing(ElectricityReading::getTime));
+//        return readings;
+//    }
+}
