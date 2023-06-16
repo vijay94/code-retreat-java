f6e853191c5a2ae5eaf961e2661ba6c4a6a0e8d9
Vijay S <vijays@Vijays-MacBook-Pro.local>
2023-06-16 14:18:21 +0530

    Add seed data for employees

--- src/main/java/uk/tw/offboarding/SeedingApplicationDataConfiguration.java
+++ src/main/java/uk/tw/offboarding/SeedingApplicationDataConfiguration.java
@@ -1,49 +1,44 @@
 package uk.tw.offboarding;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import uk.tw.offboarding.domain.Employee;
+import uk.tw.offboarding.domain.Grade;
+import uk.tw.offboarding.domain.Office;
+import uk.tw.offboarding.domain.Role;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 @Configuration
 public class SeedingApplicationDataConfiguration {
-//
-//    private static final String MOST_EVIL_PRICE_PLAN_ID = "price-plan-0";
-//    private static final String RENEWABLES_PRICE_PLAN_ID = "price-plan-1";
-//    private static final String STANDARD_PRICE_PLAN_ID = "price-plan-2";
-//
-//    @Bean
-//    public List<PricePlan> pricePlans() {
-//        final List<PricePlan> pricePlans = new ArrayList<>();
-//        pricePlans.add(new PricePlan(MOST_EVIL_PRICE_PLAN_ID, "Dr Evil's Dark Energy", BigDecimal.TEN, emptyList()));
-//        pricePlans.add(new PricePlan(RENEWABLES_PRICE_PLAN_ID, "The Green Eco", BigDecimal.valueOf(2), emptyList()));
-//        pricePlans.add(new PricePlan(STANDARD_PRICE_PLAN_ID, "Power for Everyone", BigDecimal.ONE, emptyList()));
-//        return pricePlans;
-//    }
-//
-//    @Bean
-//    public Map<String, List<ElectricityReading>> perMeterElectricityReadings() {
-//        final Map<String, List<ElectricityReading>> readings = new HashMap<>();
-//        final ElectricityReadingsGenerator electricityReadingsGenerator = new ElectricityReadingsGenerator();
-//        smartMeterToPricePlanAccounts()
-//                .keySet()
-//                .forEach(smartMeterId -> readings.put(smartMeterId, electricityReadingsGenerator.generate(20)));
-//        return readings;
-//    }
-//
-//    @Bean
-//    public Map<String, String> smartMeterToPricePlanAccounts() {
-//        final Map<String, String> smartMeterToPricePlanAccounts = new HashMap<>();
-//        smartMeterToPricePlanAccounts.put("smart-meter-0", MOST_EVIL_PRICE_PLAN_ID);
-//        smartMeterToPricePlanAccounts.put("smart-meter-1", RENEWABLES_PRICE_PLAN_ID);
-//        smartMeterToPricePlanAccounts.put("smart-meter-2", MOST_EVIL_PRICE_PLAN_ID);
-//        smartMeterToPricePlanAccounts.put("smart-meter-3", STANDARD_PRICE_PLAN_ID);
-//        smartMeterToPricePlanAccounts.put("smart-meter-4", RENEWABLES_PRICE_PLAN_ID);
-//        return smartMeterToPricePlanAccounts;
-//    }
-//
-//    @Bean
-//    @Primary
-//    public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
-//        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
-//        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-//        return objectMapper;
-//    }
+
+    @Bean
+    public List<Employee> getEmployees() throws Exception {
+        List<Employee> employees = new ArrayList<>();
+        int index = 0;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = sdf.parse("2018-12-01");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        for (Office office: Office.values()) {
+            for (Role role: Role.values()) {
+                for (Grade grade: Grade.values()) {
+                    Employee employee = new Employee();
+                    employee.setID(++index);
+                    employee.setGrade(grade);
+                    employee.setName("Employee "+index);
+                    employee.setOffice(office);
+                    employee.setRole(role);
+                    cal.add(Calendar.DAY_OF_MONTH, index);
+                    employee.setHireDate(cal.getTime());
+                    employees.add(employee);
+                }
+            }
+        }
+        return employees;
+    }
 }
--- src/main/java/uk/tw/offboarding/domain/Employee.java
+++ src/main/java/uk/tw/offboarding/domain/Employee.java
@@ -1,7 +1,12 @@
 package uk.tw.offboarding.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
 public class Employee {
     private int ID;
     private String name;
